020fc6f0515fa7ae7b70903be7670d1d87693118
Jakub Neukirch <jakub.neukirch@gmail.com>
2019-08-28 19:21:24 +0200

    setup styles

--- app/src/main/java/pl/jakubneukirch/currencycalculator/MainActivity.kt
+++ app/src/main/java/pl/jakubneukirch/currencycalculator/MainActivity.kt
@@ -1,7 +1,7 @@
 package pl.jakubneukirch.currencycalculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
